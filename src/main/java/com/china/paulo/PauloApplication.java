c08a4ed69057cdeeee32dc625ec4b3b65755782c
kefan.qu <qukefanjava@163.com>
2020-09-29 17:36:51 +0800

    feat(project): new project
    build project and init

---
+++ src/main/java/com/china/paulo/PauloApplication.java
@@ -0,0 +1,13 @@
+package com.china.paulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PauloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/china/paulo/PauloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.china.paulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
